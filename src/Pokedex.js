5c3999b81d17c177e6febf7f752e60a20a5e7bad
manosakpujiha <manos.akpujiha@gmail.com>
2023-02-06 04:15:18 -0500

    fix

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -5,10 +5,10 @@ import './Pokedex.css'
 class Pokedex extends Component {
   render() {
     const {pokemon, hand, isWinner} = this.props;
-    let title = isWinner ? 'Pokedex-win': 'Pokedex-loose';
+    let title = isWinner ? 'Pokedex-win': 'Pokedex-lose';
     return (
       <div className='Pokedex'>
-          <h3 className={title}>Hand {hand} {isWinner ? '(Winner)' : '(Looser)'} </h3>
+          <h3 className={title}>Hand {hand} {isWinner ? '(Winner)' : '(Loser)'} </h3>
           <div>Total Experience: {this.props.exp}</div>
           <div className='Pokedex-cards'> 
             {pokemon.map((p, index) => <Pokecard pokemon={p} key={index}/>)}
